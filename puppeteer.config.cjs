b36e9b2fd6a29ef2c5e90cdd2764c190f3618603
AudricUMons <251638@umons.ac.be>
2025-07-20 20:32:15 +0200

    ffe

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,6 +1,9 @@
-// puppeteer.config.cjs
+// .puppeteerrc.cjs
 const { join } = require('path');
-/** @type {import('puppeteer').Configuration} */
+
+/** @type {import("puppeteer").Configuration} */
 module.exports = {
+  // À l’installation comme au runtime,
+  // Puppeteer ira chercher Chromium dans ce dossier
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };

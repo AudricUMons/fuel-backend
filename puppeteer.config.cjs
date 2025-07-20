7ffec5c449dbebd5f880ecad3bf422cc44623c19
AudricUMons <251638@umons.ac.be>
2025-07-20 20:57:15 +0200

    azaomazeaeae

--- puppeteer.config.cjs
+++
@@ -1,8 +0,0 @@
-// puppeteer.config.cjs
-const { join } = require('path');
-
-/** @type {import('puppeteer').Configuration} */
-module.exports = {
-  // au runtime, Puppeteer ira chercher le binaire ici
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
